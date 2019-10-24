798f7cbbd8acb2d13f1e25de473baab23d1f9a79
Surucobe <garudahorror25@gmail.com>
2019-10-24 12:57:09 -0400

    pressure

--- proto_portafolio/index.js
+++ proto_portafolio/index.js
@@ -2,7 +2,7 @@ const $logo = document.getElementById('logo')
 
 //$logo.onclick = () => $logo.style = 'color: white;'
 
-window.addEventListener('click', (event) => {
+$logo.addEventListener('click', (event) => {
   if(event.target.style.color != 'white'){
     $logo.style = 'color: white;'
   }else{
